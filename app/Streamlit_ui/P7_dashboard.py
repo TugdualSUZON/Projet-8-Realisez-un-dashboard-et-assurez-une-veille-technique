8dd248e214c1cb838c85938eeaf7dc159270e6d3
TugdualSUZON <tugdual.suzon@gmail.com>
2026-02-20 15:15:27 +0100

    Màj
    Màj correctif pour faire fonctionner le model, UI fonctionnel, création un fichier teste dans le jupyter d'entrainement des modèles

--- app/Streamlit_ui/P7_dashboard.py
+++ app/Streamlit_ui/P7_dashboard.py
@@ -8,8 +8,15 @@ from io import StringIO
 
 # Fonctions
 def request_prediction(df):
-    # Charger la variables d'environnement pour render
-    MLFLOW_URL = os.getenv("MLFLOW_URL")
+    # donner l'URL
+    ## Fonctionnement tout en local
+    MLFLOW_URL = "http://127.0.0.1:5000" # Pour l'utilisation en local
+    
+    ## Fonctionnement en loca et serveur cloud
+    #MLFLOW_URL = "http://projet-7-implementez-un-modele-de-scoring.onrender.com"
+    
+    ## fonctionnement tous cloud
+    #MLFLOW_URL = os.getenv("MLFLOW_URL")
 
     # Formater le csv pour l'envoi au modèle
     df_line = df.iloc[[0], :]  # votre ligne
@@ -19,10 +26,10 @@ def request_prediction(df):
     #csv_data = csv_buffer.getvalue() # Pour voir ce qu'il y a dans le fichier csv produit, peux aussi être passer à request
     
     response = requests.post(
-        # url="http://127.0.0.1:5000/invocations", # Pour l'utilisation en local
-        url=f"{MLFLOW_URL}/invocations",
+        url=f"{MLFLOW_URL}/invocations", 
         data=csv_buffer,
-        headers={"Content-Type": "text/csv"}
+        headers={"Content-Type": "text/csv"},
+        verify=False
     )
     
     return response.status_code, response.json()
@@ -31,17 +38,25 @@ def main():
     uploaded_file = st.file_uploader("Déposé ici le fichier d'information clients en format csv", type="csv")
     if uploaded_file is not None:
         # Charger le fichier en mémoire
-        raw = pd.read_csv(uploaded_file, sep=",")
+        raw = pd.read_csv(uploaded_file)
         # Vérifier l'upload
         st.write(raw)
-    
+
+        df = copy.copy(raw)
+        SK_ID_CURR = df["SK_ID_CURR"]
+        SK_ID_CURR = SK_ID_CURR.values[0]
+        del df["SK_ID_CURR"]
+        st.write(f"Identifiant client : {SK_ID_CURR}")
+
+        
         # Préprocess
         ## Gestion de l'annomalie dans "DAYS_EMPLOYED_ANOM"
-        df = copy.copy(raw)
-        df['DAYS_EMPLOYED_ANOM'] = 0
-        mask = df["DAYS_EMPLOYED"] == 365243
-        df.loc[mask, "DAYS_EMPLOYED_ANOM"] = 1
-        df.loc[mask, "DAYS_EMPLOYED"] = np.nan
+        #df = copy.copy(raw)
+        #df['DAYS_EMPLOYED_ANOM'] = 0
+        #mask = df["DAYS_EMPLOYED"] == 365243
+        #df.loc[mask, "DAYS_EMPLOYED_ANOM"] = 1
+        #df.loc[mask, "DAYS_EMPLOYED"] = np.nan
+        
         
     predict_btn = st.button('Prédire')
     
@@ -49,10 +64,15 @@ def main():
         pred = None
         response = None
         response, pred_dict = request_prediction(df)
-        pred = pred_dict["predictions"][0][1]
-        st.write("Rique que le client ne rembouse par {:.2f}".format(pred)
+    
+        pred = pred_dict["predictions"][0]
+        if pred == 0:
+            st.write(f"Le client {SK_ID_CURR} appartient a la catégorie 0, le modèle prévoit qu'il remboursera son crédit"
                 )
-        st.write(f"Réponse de l'API {response}")
+        elif pred == 1:
+            st.write(f"Le client {SK_ID_CURR} appartient a la catégorie 1, le modèle prévoit qu'il ne remboursera pas son crédit"
+                )
+        #st.write(f"Réponse de l'API {response}")
         
 """
 # Affectation des crédit
